592e92d08fe2e8e633070c1a37b5e8c37292a0ed
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-12 14:10:04 -0300

    v-007 : Repositories update

--- src/main/java/com/payhandler/asher/payhandlerspringboot/repositories/TransactionRepository.java
+++ src/main/java/com/payhandler/asher/payhandlerspringboot/repositories/TransactionRepository.java
@@ -1,7 +1,9 @@
 package com.payhandler.asher.payhandlerspringboot.repositories;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.payhandler.asher.payhandlerspringboot.domain.transaction.Transaction;
+import com.payhandler.asher.payhandlerspringboot.services.UserService;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
--- src/main/java/com/payhandler/asher/payhandlerspringboot/repositories/UserRepository.java
+++ src/main/java/com/payhandler/asher/payhandlerspringboot/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByDocument(String document);
 
-    Optional<User> findById(String Long);
+    Optional<User> findById(Long id);
 }
