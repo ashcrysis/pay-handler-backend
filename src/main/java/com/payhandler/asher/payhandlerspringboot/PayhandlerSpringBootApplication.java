861881bcdc4afbccbc9f2cb1e8a4cfcd8e0683a7
asher gabriela <109311931+asheryyy@users.noreply.github.com>
2023-10-12 12:54:30 -0300

    v-000 - Initial Commit

---
+++ src/main/java/com/payhandler/asher/payhandlerspringboot/PayhandlerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.payhandler.asher.payhandlerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhandlerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayhandlerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payhandler/asher/payhandlerspringboot/PayhandlerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payhandler.asher.payhandlerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayhandlerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
